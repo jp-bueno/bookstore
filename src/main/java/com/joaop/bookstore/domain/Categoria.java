0c30b7f2f89e02081adc3e55e5e4e8c0cd7a3eae
João Pedro Bueno <joaopedrobueno2910@gmail.com>
2022-04-02 19:45:42 -0300

    domain class init

---
+++ src/main/java/com/joaop/bookstore/domain/Categoria.java
@@ -0,0 +1,70 @@
+package com.joaop.bookstore.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+
+public class Categoria {
+
+    private Integer id;
+    private String nome;
+    private String descricao;
+
+    private List<Livro> livros = new ArrayList<>();
+
+    public Categoria(){
+        super();
+    }
+
+    public Categoria(Integer id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(List<Livro> livros) {
+        this.livros = livros;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
