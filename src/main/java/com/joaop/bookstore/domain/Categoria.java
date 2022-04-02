955d5a08316a8ec47783bb45970f584282013992
João Pedro Bueno <joaopedrobueno2910@gmail.com>
2022-04-02 19:52:50 -0300

    finalizando as classes de dominio

--- src/main/java/com/joaop/bookstore/domain/Categoria.java
+++ src/main/java/com/joaop/bookstore/domain/Categoria.java
@@ -1,16 +1,23 @@
 package com.joaop.bookstore.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@Entity// pode criar uma tabela para o banco de dados
+public class Categoria implements Serializable {
 
-public class Categoria {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
     public Categoria(){
--- src/main/java/com/joaop/bookstore/domain/Livro.java
+++ src/main/java/com/joaop/bookstore/domain/Livro.java
@@ -1,14 +1,23 @@
 package com.joaop.bookstore.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+@Entity
+public class Livro implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
     public Livro() {
