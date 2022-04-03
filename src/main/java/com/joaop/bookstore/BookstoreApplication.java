642dff248e1dd3c43ba0c5ffbe308820b30d0c31
João Pedro Bueno <joaopedrobueno2910@gmail.com>
2022-04-02 22:04:33 -0300

    Instância de forma automática da base de dados

--- src/main/java/com/joaop/bookstore/BookstoreApplication.java
+++ src/main/java/com/joaop/bookstore/BookstoreApplication.java
@@ -1,13 +1,40 @@
 package com.joaop.bookstore;
 
+import com.joaop.bookstore.domain.Categoria;
+import com.joaop.bookstore.domain.Livro;
+import com.joaop.bookstore.repository.CategoriaRepository;
+import com.joaop.bookstore.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BookstoreApplication {
+//ComandLineRunner toda vez que você starta a implementação ela executa o que está no método
+public class BookstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "livros de TI");
+
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Loren Ipsun", cat1);
+
+		// Categoria conheceu os livros e os livros conheceram a categoria
+		cat1.getLivros().addAll(Arrays.asList(l1));
+
+		// Salvar dados no banco de dados
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+	}
 }
